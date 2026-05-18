a547575d9cde1275c4ed0fab249b65caa8f1f2aa
FieldofAction <alfred@daniel-dickson.org>
2026-05-18 16:52:45 -0700

    Update Superconscious.jsx

--- src/components/Superconscious.jsx
+++ src/components/Superconscious.jsx
@@ -125,7 +125,7 @@ export default function Superconscious() {
               <img
                 src={d.thumb}
                 alt={`Drawing ${d.id}`}
-                loading="lazy"
+                loading="eager"
                 className="sc-grid-img"
               />
             </div>
